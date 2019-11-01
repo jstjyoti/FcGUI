474ee465aa0bcca41586447ad04e43fa0b442914
vivekburman <vivekburman1997@gmail.com>
2019-11-01 12:59:59 +0530

    comments written on aside folder files, on what to do

---
+++ src/assets/js/aside/aside.js
@@ -0,0 +1,8 @@
+// will be a class
+// will be fired from main.js based on clicks
+// its job:- 
+// 1. take input from asideData.js
+// 2. create a ASIDE HTML DOM FORM
+// 3. ON submit click
+// 4. Will extract all attributes and pass it to chart.js
+// 5. chart.js will render chart based on changes
---
+++ src/assets/js/aside/asideData.js
@@ -0,0 +1,2 @@
+// create a variable for each component like canvas, chart, dataplot, caption etc..p5.BandPass()
+// inside each variable give the object as shown
