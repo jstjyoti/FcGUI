8358be68dc296e312fc2225e2ef0847bd2f86376
vivekburman <vivekburman1997@gmail.com>
2019-11-03 13:14:11 +0530

    added layout for aside and input fields

--- src/assets/js/aside/aside.js
+++ src/assets/js/aside/aside.js
@@ -1,8 +1,190 @@
-// will be a class
-// will be fired from main.js based on clicks
-// its job:- 
-// 1. take input from asideData.js
-// 2. create a ASIDE HTML DOM FORM
-// 3. ON submit click
-// 4. Will extract all attributes and pass it to chart.js
-// 5. chart.js will render chart based on changes
+function showHideDetail(e) {
+    let dom = e.target
+    const sideNav = document.querySelector('.aside-detail')
+
+    function deleteNode() {
+        while (sideNav.hasChildNodes()) {
+            sideNav.removeChild(sideNav.lastChild)
+        }
+    }
+    while (dom.tagName !== 'LI') {
+        dom = dom.parentElement
+    }
+    if (dom.classList.contains('aside-link-active')) {
+        sideNav.classList.add('aside-detail-hide')
+        deleteNode()
+        dom.classList.remove('aside-link-active')
+        return 0
+    } else {
+        // remove if someone has it
+        const nodes = Array.from(document.querySelectorAll('.aside-link-container'))
+        for (var node of nodes) {
+            if (node.classList.contains('aside-link-active')) {
+                node.classList.remove('aside-link-active')
+                break;
+            }
+        }
+        dom.classList.add('aside-link-active')
+        if (sideNav.classList.contains('aside-detail-hide') === false) {
+            deleteNode()
+        } else {
+            sideNav.classList.remove('aside-detail-hide')
+        }
+        return 1
+    }
+}
+
+function aside() {
+    const skeleton = {
+        'parent': {
+            'name': 'div',
+            'property': {
+                'class': 'aside-container'
+            },
+        },
+        'children': [{
+            'parent': {
+                'name': 'ul',
+                'property': {
+                    'class': 'aside-nav'
+                }
+            },
+            'children': [{
+                'parent': {
+                    'name': 'li',
+                    'property': {
+                        'class': 'aside-link-container'
+                    },
+                    'event': function () {
+                        // onclick will open aside-detail tab, each tab
+                        // will open a different width
+                        // will add class
+                        this.addEventListener('click', function (e) {
+                            showHideDetail(e)
+                        })
+                    }
+                },
+                'children': [{
+                    'parent': {
+                        'name': 'a',
+                        'property': {
+                            'class': 'aside-link'
+                        }
+                    },
+                    'children': [{
+                        'parent': {
+                            'name': 'i',
+                            'property': {
+                                'class': 'fa fa-table aside-icon'
+                            }
+                        }
+                    }]
+                }]
+            }, {
+                'parent': {
+                    'name': 'li',
+                    'property': {
+                        'class': 'aside-link-container'
+                    },
+                    'event': function () {
+                        // onclick will open aside-detail tab, each tab
+                        // will open a different width
+                        // will add class
+                        this.addEventListener('click', function (e) {
+                            showHideDetail(e)
+                        })
+                    }
+                },
+                'children': [{
+                    'parent': {
+                        'name': 'a',
+                        'property': {
+                            'class': 'aside-link'
+                        }
+                    },
+                    'children': [{
+                        'parent': {
+                            'name': 'i',
+                            'property': {
+                                'class': 'fa fa-bar-chart aside-icon'
+                            }
+                        }
+                    }]
+                }]
+            }, {
+                'parent': {
+                    'name': 'li',
+                    'property': {
+                        'class': 'aside-link-container'
+                    },
+                    'event': function () {
+                        // onclick will open aside-detail tab, each tab
+                        // will open a different width
+                        // will add class
+                        this.addEventListener('click', (e) => {
+                            if (showHideDetail(e) === 1) {
+                                customize()
+                            }
+                        })
+                    }
+                },
+                'children': [{
+                    'parent': {
+                        'name': 'a',
+                        'property': {
+                            'class': 'aside-link'
+                        }
+                    },
+                    'children': [{
+                        'parent': {
+                            'name': 'i',
+                            'property': {
+                                'class': 'fa fa-sliders aside-icon'
+                            }
+                        }
+                    }]
+                }]
+            }, {
+                'parent': {
+                    'name': 'li',
+                    'property': {
+                        'class': 'aside-link-container'
+                    },
+                    'event': function () {
+                        // onclick will open aside-detail tab, each tab
+                        // will open a different width
+                        // will add class
+                        this.addEventListener('click', function (e) {
+                            showHideDetail(e)
+                        })
+                    }
+                },
+                'children': [{
+                    'parent': {
+                        'name': 'a',
+                        'property': {
+                            'class': 'aside-link'
+                        }
+                    },
+                    'children': [{
+                        'parent': {
+                            'name': 'i',
+                            'property': {
+                                'class': 'fa fa-download aside-icon'
+                            }
+                        }
+                    }]
+                }]
+            }]
+        }, {
+            'parent': {
+                'name': 'div',
+                'property': {
+                    'class': 'aside-detail aside-detail-hide'
+                }
+            }
+        }]
+    }
+    const aside = document.getElementById('aside')
+    aside.appendChild(render(skeleton))
+}
---
+++ src/assets/js/index.js
@@ -0,0 +1,7 @@
+(function index() {
+    // header()
+    // nav()
+    aside()
+    // chart()
+    // footer()
+})()
