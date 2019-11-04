60eea341ba46a177f053344beda5ca1cd09bee6c
Jyoti Mishra <37954338+jstjyoti@users.noreply.github.com>
2019-11-04 12:58:47 +0530

    Update asideData.js

--- src/assets/js/aside/asideData.js
+++ src/assets/js/aside/asideData.js
@@ -1,9 +1,6 @@
 // create a variable for each component like canvas, chart, dataplot, caption etc..p5.BandPass()
 // inside each variable give the object as shown
 
-
-
-
 const dataplot = {
     features: [{
         label: 'Colors',
@@ -504,7 +501,6 @@ const plotValue = {
         note: '',
         defaultActive: '0',
     }]
-=======
 const canvas = {
     'padding': [{
         'label': 'Left & Right Padding',
