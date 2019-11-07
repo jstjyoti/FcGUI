6b63c104fa6243fab4351d4a531988b38e78357a
Srishti <srishjaiz@gmail.com>
2019-11-07 13:19:00 +0530

    resolve conflict

--- src/assets/js/aside/asideData.js
+++ src/assets/js/aside/asideData.js
@@ -1,6 +1,6 @@
 // create a variable for each component like canvas, chart, dataplot, caption etc..p5.BandPass()
 // inside each variable give the object as shown
-
+//for resolving conflict
 function showOrHide(state, nodes) {
     for (var node of nodes) {
         document.getElementById(node).disabled = state
